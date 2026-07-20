a30a1a436dbfbf35c04c669508ddb0a171d4f4b7
Abdelrahman-Mahmoud-Elsaeed <abdo567465@gmail.com>
2026-07-20 17:27:16 +0300

    updates

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "app" {
   name                 = var.repository_name
   image_tag_mutability = "IMMUTABLE"
-
+  force_delete = true
   image_scanning_configuration {
     scan_on_push = true
   }

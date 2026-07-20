58b63544293dfac09d674354ac696783badc6110
Abdelrahman-Mahmoud-Elsaeed <abdo567465@gmail.com>
2026-07-20 16:38:52 +0300

    updates

--- server.ts
+++ server.ts
@@ -1,7 +1,17 @@
-import {app} from "./src/app.ts";
-import {PORT} from "./src/config/env.ts";
+import { app } from "./src/app.ts";
+import { PORT } from "./src/config/env.ts";
 
+app.get('/', (req, res) => {
+  res.status(200).send('OK');
+});
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+const HOST: string = '0.0.0.0';
+const portNumber: number = Number(PORT) || 3000;
+
+app.listen(portNumber, HOST, () => {
+  console.log(`Server listening at http://${HOST}:${portNumber}`);
 });

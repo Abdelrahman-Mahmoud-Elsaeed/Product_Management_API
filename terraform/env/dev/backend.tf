3c319b1d50487a6b8049dcd036e935109726169c
Abdelrahman-Mahmoud-Elsaeed <abdo567465@gmail.com>
2026-07-20 16:59:41 +0300

    updates

--- terraform/env/dev/backend.tf
+++ terraform/env/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket" # Must be globally unique
+    bucket         = "terraform-state-bucket-468997136367-us-east-1-an"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
